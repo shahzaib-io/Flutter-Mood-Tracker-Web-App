564ff42c18dd0186b1e54fe225473e9d75d9430b
shahz-dev <shahzaib.dev001@gmail.com>
2026-05-16 08:15:56 +0500

    Created Just Empty Mood Tracker Screen
    Created Mood Tracker Screen with stateful widget inside screens folder and called that Mood Tracker Screen inside main.dart as home screen

---
+++ lib/screens/mood_tracker_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoodTrackerScreen extends StatefulWidget {
+  const MoodTrackerScreen({super.key});
+
+  @override
+  State<MoodTrackerScreen> createState() => _MoodTrackerScreenState();
+}
+
+class _MoodTrackerScreenState extends State<MoodTrackerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
