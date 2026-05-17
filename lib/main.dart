6a34005971c51b63517814f65507e02cd5c3f6c2
shahz-dev <shahzaib.dev001@gmail.com>
2026-05-17 19:20:54 +0500

    Turned Off Debug Mode Banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mood Tracker',
       theme: ThemeData(
         // This is the theme of your application.
