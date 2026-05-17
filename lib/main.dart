57cb6ad534e01c7ccb9896062362b76d0690a5a7
shahz-dev <shahzaib.dev001@gmail.com>
2026-05-17 19:24:22 +0500

    Changed Color Scheme
    Added Material3 and colorSchemeSeed to Colors.blue in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
       ),
       home: const MoodTrackerScreen(),
     );
